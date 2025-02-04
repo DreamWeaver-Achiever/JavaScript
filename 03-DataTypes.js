a30ab1945ff18462bc25d1f006390186fb0ebeb9
DreamWeaver-Achiever <sakm2160@gmail.com>
2025-02-04 18:42:50 +0000

    Data Types

---
+++ 03-DataTypes.js
@@ -0,0 +1,15 @@
+"use strict";
+
+console.log("Hello World!")
+
+//alert(3+3) //Can't be used in the vscode and it only works on the browser.
+
+let name = "John Doe"
+console.log(typeof name)
+
+let age = 42
+console.log(typeof age)
+
+console.log(typeof null)
+
+console.log(typeof undefined)
