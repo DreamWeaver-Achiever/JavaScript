f56ce62bd141d3ba83b27dd8262fb76fcc416ed8
DreamWeaver-Achiever <sakm2160@gmail.com>
2025-02-04 19:36:37 +0000

    Data Type Conversions

---
+++ 04-DataTypeConversion.js
@@ -0,0 +1,23 @@
+let name = "John Doe"
+
+console.log(typeof name)
+console.log(typeof(name))
+
+let convertNameIntoNumber = Number(name)
+console.log(typeof convertNameIntoNumber)
+
+let age  = 33
+console.log(typeof age)
+let ageSting = String(age)
+console.log(typeof ageString)
+let ageNan = "33abc"
+console.log(typeof ageNan)
+
+console.log(Boolean(age))
+
+let isLoggedIn = 0
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+let someNumber = 33
+console.log()
