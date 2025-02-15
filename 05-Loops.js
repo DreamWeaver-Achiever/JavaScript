a0c7859dcbfd90bfddeaaae497cf8181518bddfe
Sakshi M <sakm2160@gmail.com>
2025-02-15 00:58:35 +0000

    Loops

---
+++ 05-Loops.js
@@ -0,0 +1,18 @@
+console.log("'for' loop:")
+for(let i=1; i<6; i++) {
+    console.log(i);
+}
+
+console.log("\n'while' loop:")
+let j = 6;
+while(j<=10) {
+    console.log(j);
+    j++;
+}
+
+console.log("\n'do-while' loop:")
+let k = 11; 
+do{
+    console.log(k);
+    k++;
+} while (k<=15)
