b4d7346592aab892ec9eab9f306b084c06e82a19
Sakshi M <sakm2160@gmail.com>
2025-02-22 06:37:26 +0000

    Arrow function, Callback function, Returning function

--- 07-Functions.js
+++ 07-Functions.js
@@ -13,3 +13,43 @@ function table(n) {
     
 }
 table(2);
+
+/*function expression -> A function expression defines a function as part of an expression, typically by assigning it to a variable. It's not hoisted, so it must be defined before it's called. */
+let expression = function add(name) { 
+    return `Hello, ${name}`;
+}
+console.log("Function expression example: ", expression("Jane doe"));
+
+/*Introduced in ES6, arrow functions provide a concise syntax for writing functions, especially for short, anonymous functions.*/
+var arrowFunction = (name) => {
+    return `Hello, ${name}`;
+}
+console.log("Arrow function example: ", arrowFunction("Jane Doe"));
+
+/*Callback Functions -> JavaScript functions can be passed as arguments to other functions, commonly known as callback functions. They're extensively used in asynchronous programming and event handling.*/
+function processData(data) {
+    console.log(`Processing data: ${data}`);
+}
+function fetchData(callbackFunction) {
+    // Simulated asynchronous operation
+    setTimeout(() => {
+        const data = "Data fetched successfully";
+        callbackFunction(data);
+    }, 2000);
+}
+fetchData(processData); // Output: Processing data: Data fetched successfully
+
+/*Returning Functions -> 
+In JavaScript, functions can return other functions, enabling the creation of higher-order functions. */
+function multiplier(factor) {
+    return function(number) {
+        return number * factor;
+    };
+}
+console.log("Multiplier: ", multiplier(2));
+
+/*Built-in Functions
+JavaScript comes with several built-in functions, such as setTimeout, setInterval, and addEventListener, which take functions as arguments.*/
+setTimeout(() => {
+    console.log("Delayed function");
+}, 1000);
